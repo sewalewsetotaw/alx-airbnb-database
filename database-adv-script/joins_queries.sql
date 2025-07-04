fa2de1c1d723b9d631caf977a467062ea190afb9
SewalewS29 <sewalews29@gmail.com>
2025-07-05 00:11:50 +0300

    Add order by to the left join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -23,7 +23,8 @@ SELECT
     r.comment,
     r.user_id AS reviewer_id
 FROM Property p
-LEFT JOIN Review r ON p.property_id = r.property_id;
+LEFT JOIN Review r ON p.property_id = r.property_id
+ORDER BY r.rating;
 
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
