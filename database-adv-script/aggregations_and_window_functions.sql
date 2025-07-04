9678d2ade09b245fd07d3817cc5a1a53f284e275
SewalewS29 <sewalews29@gmail.com>
2025-07-05 01:34:58 +0300

    adjust formatting

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -4,12 +4,8 @@ SELECT u.user_id,
        u.first_name,
        u.last_name,
        Count(b.booking_id) total_bookings
-FROM   "user" u
-       LEFT JOIN booking b
-              ON u.user_id = b.user_id
-GROUP  BY u.user_id,
-          u.first_name,
-          u.last_name
+FROM   "user" u LEFT JOIN booking b  ON u.user_id = b.user_id
+GROUP  BY u.user_id, u.first_name, u.last_name
 ORDER  BY total_bookings DESC  
 
 -- 2. Window Function: Rank properties by total number of bookings
@@ -18,15 +14,8 @@ SELECT p.property_id,
        p."name"  property_name,
        p.description,
        Count(b.booking_id) total_bookings,
-       Row_number()
-         OVER(
-           ORDER BY Count(b.booking_id) DESC),
-       Rank()
-         OVER(
-           ORDER BY Count(b.booking_id) DESC)
-FROM   property p
-       LEFT JOIN booking b
-              ON p.property_id = b.property_id
+       Row_number() OVER( ORDER BY Count(b.booking_id) DESC),
+       Rank() OVER( ORDER BY Count(b.booking_id) DESC)
+FROM   property p LEFT JOIN booking b ON p.property_id = b.property_id
 GROUP  BY p.property_id
-ORDER  BY p.property_id,
-          p."name" DESC  
+ORDER  BY p.property_id, p."name" DESC  
